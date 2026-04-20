d0a3a5dd4703b9d2e1b52d8a177aed82c52e07af
Adrian <adriansnhagyard@gmail.com>
2026-04-20 17:16:08 -0400

    updates

--- docs/assets/js/view.js
+++ docs/assets/js/view.js
@@ -1,13 +1,19 @@
-function dragStart(event) {
-    event.dataTransfer.setData("text", event.target.id);
-}
-
-function dragStop(event) {
-    event.preventDefault();
-}
-
-function dragDrop(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
+let slots = document.querySelectorAll(".slot");
+let draggables = document.querySelectorAll("[draggable=true]");
+function initPage() {
+    slots.forEach(slot => {
+        slot.addEventListener("ondrop", event => {
+            event.preventDefault();
+            const data = event.dataTransfer.getData("text");
+            event.target.appendChild(document.getElementById(data));
+        });
+        slot.addEventListener("ondragover", event => {
+            event.preventDefault();
+        });
+    });
+    draggables.forEach(draggable => {
+        draggable.addEventListener("ondragstart", event => {
+            event.dataTransfer.setData("text", event.target.id);
+        });
+    });
 }
